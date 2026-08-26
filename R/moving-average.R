e4b058564222155f63b39745610d2eccba252931
ethan-mathews24 <ethansimonmathews@gmail.com>
2026-08-26 15:25:09 -0700

    updating

--- R/moving-average.R
+++ R/moving-average.R
@@ -4,15 +4,14 @@ library(tidyverse)
 
 # Moving Averages ---------------------------------------------------------
 
-
 moving_average <- function(filtered_data) {
   # Initialize a tibble to contain the results
   result <- tibble(
     window_start = seq(ymd("1988-01-05"), ymd("1994-12-26"), by = "9 weeks"),
     k_mgl = NA,
     mg_mgl = NA,
-    no3_ugl = NA, 
-    ca_mgl = NA, 
+    no3_ugl = NA,
+    ca_mgl = NA,
     nh4_ugl = NA
     # Fill in the rest of the ions
   )
@@ -25,7 +24,8 @@ moving_average <- function(filtered_data) {
 
     # Create a logical vector, called "in_window", that says which samples are inside the window
     # Hint: you'll compare sample dates to the start and end of the window
-    in_window <- filtered_data$Sample_Date >= w1 & filtered_data$Sample_Date < w2
+    in_window <- filtered_data$Sample_Date >= w1 &
+      filtered_data$Sample_Date < w2
 
     # Use indexing to pull out the ion concentrations that fall inside the window
     k_window <- filtered_data$K[in_window]
@@ -38,10 +38,10 @@ moving_average <- function(filtered_data) {
     # Calculate the mean of each ion concentration and fill in the result
     result$k_mgl[i] <- mean(k_window, na.rm = TRUE)
     result$mg_mgl[i] <- mean(mg_window, na.rm = TRUE)
-    result$no3_ugl[i] <- mean(nit_window, na.rm = TRUE) 
-    result$nh4_ugl[i] <- mean(amon_window, na.rm = TRUE) 
+    result$no3_ugl[i] <- mean(nit_window, na.rm = TRUE)
+    result$nh4_ugl[i] <- mean(amon_window, na.rm = TRUE)
     result$ca_mgl[i] <- mean(calc_window, na.rm = TRUE)
   }
-  
+
   return(result)
 }
