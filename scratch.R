5924a04c1c993d932ffeb614d5478f80fbeb273b
ethan-mathews24 <ethansimonmathews@gmail.com>
2026-08-27 10:51:57 -0700

    update

--- scratch.R
+++ scratch.R
@@ -1,20 +1,17 @@
-
-
 library(tidyverse)
 library(dplyr)
 
 source("R/moving-average.R")
 
 
-
-# Read in Data 
+# Read in Data
 
 BQ1 <- read_csv("data/QuebradaCuenca1-Bisley.csv")
 
 BQ2 <- read_csv("data/QuebradaCuenca2-Bisley.csv")
 
 BQ3 <- read_csv("data/QuebradaCuenca3-Bisley.csv")
-  
+
 PRM <- read_csv("data/RioMameyesPuenteRoto.csv")
 
 problems(BQ1)
@@ -23,21 +20,34 @@ problems(BQ1)
 combined_data <- bind_rows(BQ1, BQ2, BQ3, PRM)
 
 
-
-
-# select the important rows 
-clean_combined_data <- combined_data |> 
-  select("Sample_ID", "Code", "Sample_Date", "NO3-N", "K", "Mg", "Ca", "NH4-N") |> 
+# select the important rows
+clean_combined_data <- combined_data |>
+  select(
+    "Sample_ID",
+    "Code",
+    "Sample_Date",
+    "NO3-N",
+    "K",
+    "Mg",
+    "Ca",
+    "NH4-N"
+  ) |>
   filter(year(Sample_Date) %in% c(1988:1994))
 
 # filtered BQ1 dataset
 
-BQ1_filtered <- BQ1 |> 
-  select("Sample_ID", "Code", "Sample_Date", "NO3-N", "K", "Mg", "Ca", "NH4-N") |> 
+BQ1_filtered <- BQ1 |>
+  select(
+    "Sample_ID",
+    "Code",
+    "Sample_Date",
+    "NO3-N",
+    "K",
+    "Mg",
+    "Ca",
+    "NH4-N"
+  ) |>
   filter(year(Sample_Date) %in% c(1988:1994))
-
-
-
 
 
 # Exploratory Data -------------------------------------------------------
@@ -53,98 +63,96 @@ BQ1_filtered <- BQ1 |>
 
 # glimpse(clean_combined_data)
 
-
-
 # Sketchy Graph ----------------------------------------------------------
 
-
-nut_long <- clean_combined_data |> 
+nut_long <- clean_combined_data |>
   pivot_longer(
     cols = c(K, Ca, Mg, `NO3-N`, `NH4-N`),
-    names_to = "nutrient", 
-    values_to = "concentration")
+    names_to = "nutrient",
+    values_to = "concentration"
+  )
 
-ggplot(data = nut_long, 
+ggplot(
+  data = nut_long,
   mapping = aes(
-  x = Sample_Date,
-  y = concentration,
-  group = nutrient, 
-  color = nutrient)) + 
-  
+    x = Sample_Date,
+    y = concentration,
+    group = nutrient,
+    color = nutrient
+  )
+) +
+
   geom_line() +
-  
-  theme_bw() + 
-  
-  labs(title = "Trash Plot", 
-  x = "Date", 
-  y = "Concentration") +
-  
+
+  theme_bw() +
+
+  labs(title = "Trash Plot", x = "Date", y = "Concentration") +
+
   scale_color_manual(
     name = "Nutrient",
     values = c(
-      "Ca"    = "red",
-      "K"     = "goldenrod",
-      "Mg"    = "forestgreen",
+      "Ca" = "red",
+      "K" = "goldenrod",
+      "Mg" = "forestgreen",
       "NH4-N" = "dodgerblue",
       "NO3-N" = "deeppink"
     ),
     labels = c(
-      "Ca"    = "Calcium",
-      "K"     = "Potassium",
-      "Mg"    = "Magnesium",
+      "Ca" = "Calcium",
+      "K" = "Potassium",
+      "Mg" = "Magnesium",
       "NH4-N" = "Ammonium",
       "NO3-N" = "Nitrate"
-    ))
-
+    )
+  )
 
 
 # Finding the Moving Average over 9 Weeks --------------------------------
 
-
-
 moving_average(BQ1_filtered)
-
 
 
 # Plots for BQ1 -----------------------------------------------------------
 
-mov_long <- moving_average |> 
+mov_long <- moving_average |>
   pivot_longer(
     cols = c(k_mgl, ca_mgl, mg_mgl, NO3_ugl, NH4_ugl),
-    names_to = "nutrient", 
-    values_to = "concentration")
+    names_to = "nutrient",
+    values_to = "concentration"
+  )
 
-ggplot(data = mov_long, 
+ggplot(
+  data = mov_long,
   mapping = aes(
-  x = window_start,
-  y = concentration,
-  group = nutrient, 
-  color = nutrient)) + 
-  
+    x = window_start,
+    y = concentration,
+    group = nutrient,
+    color = nutrient
+  )
+) +
+
   geom_line() +
-  
-  theme_bw() + 
-  
-  labs(title = "BQ1", 
-  x = "Date", 
-  y = "Concentration") +
-  
+
+  theme_bw() +
+
+  labs(title = "BQ1", x = "Date", y = "Concentration") +
+
   scale_color_manual(
     name = "Nutrient",
     values = c(
-      "ca_mgl"    = "red",
-      "k_mgl"     = "goldenrod",
-      "mg_mgl"    = "forestgreen",
+      "ca_mgl" = "red",
+      "k_mgl" = "goldenrod",
+      "mg_mgl" = "forestgreen",
       "NH4_ugl" = "dodgerblue",
       "NO3_ugl" = "deeppink"
     ),
     labels = c(
-      "ca_mgl"    = "Calcium",
-      "k_mgl"     = "Potassium",
-      "mg_mgl"    = "Magnesium",
+      "ca_mgl" = "Calcium",
+      "k_mgl" = "Potassium",
+      "mg_mgl" = "Magnesium",
       "NH4_ugl" = "Ammonium",
       "NO3_ugl" = "Nitrate"
-    )) +
-  
-  facet_wrap(~nutrient, scales = "free")
+    )
+  ) +
 
+  facet_wrap(~nutrient, scales = "free")
