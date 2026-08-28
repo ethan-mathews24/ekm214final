dce0ad8d909ca5d0bb9883dd0d1ba2768a4939b8
ethan-mathews24 <ethansimonmathews@gmail.com>
2026-08-27 17:13:33 -0700

    adding professional comments

--- 01_clean_data.R
+++ 01_clean_data.R
@@ -7,6 +7,8 @@ source("R/moving-average.R")
 
 
 # Read in Data
+# Keep only the columns we need and limit each site to the 1988-1994 study
+# period so all four datasets match up before combining
 
 BQ1 <- read_csv("data/QuebradaCuenca1-Bisley.csv") |> 
     select(
@@ -64,6 +66,8 @@ PRM <- read_csv("data/RioMameyesPuenteRoto.csv") |>
   filter(year(Sample_Date) %in% c(1988:1994))
 
 
+# Apply the moving_average() to each of the previously cleaned dataframes and then create them
+# as new objects in the environment. 
 
 BQ1_avg <- moving_average(BQ1)
 
@@ -74,11 +78,13 @@ BQ3_avg <- moving_average(BQ3)
 PRM_avg <- moving_average(PRM)
 
 
+# Combine all four sites into one table so they can be compared side by side 
 combined_avg_data <- bind_rows(BQ1_avg, BQ2_avg, BQ3_avg, PRM_avg)
 
 
 
-
+# Reshape the data and rename/reorder the nutrients and sites to match how
+# they were presented in Figure 3
 fig3 <- combined_avg_data |> 
   pivot_longer(
 cols = c(k_mgl, ca_mgl, mg_mgl, no3_ugl, nh4_ugl),
@@ -94,6 +100,6 @@ values_to = "concentration",
   labels = c("PRM", "BQ1", "BQ2", "BQ3")      
 ))
 
-
+# Save the cleaned data so it can be used to make figures in other scripts
 write_csv(fig3, "output/clean_data.csv")
 
